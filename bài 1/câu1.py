e90cd80c9df77b136109408184f0a42eba942dae
ngodaccuong311006-stack <ngodaccuong311006@gmail.com>
2025-12-14 16:45:30 +0700

    Delete bài 1 directory

--- bài 1/câu1.py
+++
@@ -1,9 +0,0 @@
-print("sinh viên: ngô đắc cường")
-print("msv: 245752021610035")
-n1 = int(input("enter n1 value"))
-n2 = int(input("enter n2 value"))
-
-if n1 > n2:
-    print("n1 is big")
-else:
-    print("n2 is big")
